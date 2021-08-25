764f171bdd619a599f15eae461a2c44ed366ffed
Pedro Bomfim <pedromarinho1551@gmail.com>
2021-08-25 18:36:38 -0400

    Atividade001
    Leitura de um nome com exibição de boas vindas.

--- 001.py
+++ 001.py
@@ -1,14 +1,23 @@
+# Definindo Métodos
 def linha():
     print("\033[0;33m~\033[m"*40)
 
 def leiaSTR():
-    a = str(input("\033[0;32mDigite o seu nome: \033[m"))
+    while True:
+        try: 
+            a = str(input("\033[0;32mDigite o seu nome: \033[m")).strip().upper()
+        except:
+            print("\033[0;31m< ERRO > Falha na leitura dos dados < ERRO >\033[m")
+            continue
+        else:
+            break
     return a
 
 def boasvindas(txt):
     linha()
-    print(f"\033[0;33m{'BOAS VINDAS AO SISTEMA'} {txt}\033[m".center(40).upper())
+    print(f"\033[0;33mBOAS VINDAS AO SISTEMA {txt}\033[m".center(50))
     linha()
 
+# Programa Principal
 nome = leiaSTR()
 boasvindas(nome)
