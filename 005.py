4c497162c47c6949a7a56a25341f71183146888b
Pedro Bomfim <pedromarinho1551@gmail.com>
2021-08-25 19:55:44 -0400

    Atividade005
    Leitura de um número, mostrando seu dobro, triplo e raiz quadrada.

---
+++ 005.py
@@ -0,0 +1,40 @@
+from math import sqrt
+
+# Definindo Métodos
+def dobro(v):
+    d = v * 2
+    return d
+
+def triplo(v):
+    t = v * 3
+    return t
+
+def raizQuadrada(v):
+    r = sqrt(v)
+    return r
+
+def exibir():
+    print(f"O dobro de \033[0;33m{n}\033[m é \033[0;34m{dobro(n)}\033[m")
+    print(f"O triplo de \033[0;33m{n}\033[m é \033[0;34m{triplo(n)}\033[m")
+    print(f"A raiz quadrada de \033[0;33m{n}\033[m é \033[0;34m{raizQuadrada(n)}\033[m")
+
+def leiaInt():
+    while True:
+        try: 
+            l = int(input("\033[0;32mDigite um número: \033[m"))
+        except:
+            print("\033[0;31m< ERRO > Falha na leitura dos dados < ERRO >\033[m")
+            continue
+        else:
+            break
+    return l
+
+def linha():
+    print("\033[0;35m~\033[m"*40)
+
+# Programa Pricipal
+linha()
+n = leiaInt()
+linha()
+exibir()
+linha()
