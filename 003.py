c1df4733ea9b4ea6326e5a7488f4a5d068b0acef
Pedro Bomfim <pedromarinho1551@gmail.com>
2021-08-25 19:23:56 -0400

    Atividade003
    Leitura de um valor e exibição de informações sobre.

---
+++ 003.py
@@ -0,0 +1,37 @@
+# Definindo Métodos
+def exibição(v):
+    linha()
+    print(f"\033[0;32mO valor '{v}' é uma string?\033[m")
+    if v.isalpha():
+        print(f"\033[0;33mSim, '{v}' é um valor string!\033[m")
+        linha()
+    else:
+        print(f"\033[0;31mNão, '{v}' não é um valor string!\033[m")
+        linha()
+
+    print(f"\033[0;32mO valor '{v}' é um número?\033[m")
+    if v.isnumeric():
+        print(f"\033[0;33mSim, '{v}' é um valor numérico!\033[m")
+        linha()
+    else:
+        print(f"\033[0;31mNão, '{v}' não é um valor numérico!\033[m")
+        linha()
+        
+    print(f"\033[0;32mO valor '{v}' é alfanumérico?\033[m")
+    if v.isalnum():
+        print(f"\033[0;33mSim, '{v}' é um valor alfanumérico!\033[m")
+        linha()
+    else:
+        print(f"\033[0;31mNão, '{v}' não é um valor alfanumérico!\033[m")
+        linha()
+
+def leitura():
+    l = input("\033[0;32mDigite algo a ser analisado: \033[m")
+    return l
+
+def linha():
+    print("="*40)
+
+# Programa Principal
+v = leitura()
+exibição(v)
