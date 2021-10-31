c082d827ca014e2d5024a34fa2789aa84be018d6
Pedro Bomfim <pedromarinho1551@gmail.com>
2021-10-31 12:45:35 -0400

    Atualização de Arquivo
    Exercícios 008 e 015

--- 008.py
+++ 008.py
@@ -1,3 +1,5 @@
+from Biblioteca import leitura
+
 # Definindo Funções
 def tabuada(v):
     linha()
@@ -5,21 +7,10 @@ def tabuada(v):
         print(f"\033[0;32m{v}\033[m x \033[0;32m{c}\033[m = \033[0;33m{v * c}\033[m")
     linha()
 
-def lerValor():
-    while True:
-        try: 
-            l = int(input("\033[0;32mDigite um número inteiro: \033[m"))
-        except:
-            print("\033[0;31m< ERRO > Indique um número inteiro válido < ERRO >\033[m")
-            continue
-        else:
-            break
-    return l
-
 def linha():
     print("\033[0;32m~\033[m"*28)
 
 
 # Programa Principal
-n = lerValor()
+n = leitura.lerNúmInteiro()
 tabuada(n)
