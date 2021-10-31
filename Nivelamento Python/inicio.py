fbc0ad8c536caac167452c472337a805250ec128
Pedro Bomfim <pedromarinho1551@gmail.com>
2021-10-30 21:16:38 -0400

    Nivelamento
    Nivelamento em Python!

---
+++ Nivelamento Python/inicio.py
@@ -0,0 +1,14 @@
+# Iniciando nivelamento em python - Antero
+
+nome = "Bomfim"             # string
+idade = "19"                # string
+idade_número = 19           # inteiro (número)
+altura = 1.76               # float (número)
+vivo = True                 # boolean (Verdadeiro ou Falso)
+
+print(f"Seu nome é {nome}")
+print(f"Você tem a idade de {idade_número} anos")
+print(f"Possuindo altura de {altura}")
+
+print("Você tem a idade de %i anos" % idade_número)       # formatação antiga
+# print("Você tem a idade de %i anos" % idade)            # TypeERROR
