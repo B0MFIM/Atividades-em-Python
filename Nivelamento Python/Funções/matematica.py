940815db6e06c7cbe9029e043d3a1c7bfd21da8c
Pedro Bomfim <pedromarinho1551@gmail.com>
2021-10-30 21:16:19 -0400

    Nivelamento
    Nivelamento em Python!

---
+++ Nivelamento Python/Funções/matematica.py
@@ -0,0 +1,8 @@
+# Iniciando nivelamento em python - Antero
+
+# Definição de Somar: recebe x e y, retorna x + y
+
+def somar(x = 0, y = 0):
+    return float(x) + float(y)
+
+#OBS: para tornar um diretório inicializável, crie um arquivo em branco chamado __init__.py dentro da pasta que contém sua biblioteca de funções
