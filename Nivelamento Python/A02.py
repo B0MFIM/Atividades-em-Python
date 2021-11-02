4d8a96936447b30922f2a852146f26b3e5e808f7
Pedro Bomfim <pedromarinho1551@gmail.com>
2021-11-01 20:56:11 -0400

    Atualização!
    Remoção!

--- Nivelamento Python/A02.py
+++
@@ -1,5 +0,0 @@
-# Iniciando nivelamento em python - Antero
-
-fahreinheit = float(input("Insira o valor da Temperatura(°F): "))
-celsius = (fahreinheit - 32) / 1.8
-print(f"{fahreinheit}°F convertida para Celsius é {celsius:.2f}°C")
--- Nivelamento Python/Funções/matematica.py
+++
@@ -1,8 +0,0 @@
-# Iniciando nivelamento em python - Antero
-
-# Definição de Somar: recebe x e y, retorna x + y
-
-def somar(x = 0, y = 0):
-    return float(x) + float(y)
-
-#OBS: para tornar um diretório inicializável, crie um arquivo em branco chamado __init__.py dentro da pasta que contém sua biblioteca de funções
